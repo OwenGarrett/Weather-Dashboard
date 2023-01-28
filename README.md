d4553376a9700f4100d565e0aaecb50a8bb97a4b
Sheogorath9 <ogarrett189@gmail.com>
2023-01-28 17:48:48 -0500

    added basic styling and readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Weather-Dashboard
+
+Thank you for taking the time to review my submission and for providing your invaluable feedback. I am very greatful for your detailed feedback as it helps me in the development process. I am very eager to learn! 
+
+This is my second submission of this project. My favorite part of the app is the CSS styling. I enjoy it in the beginning because it helps me not only learn about all of the different elements that I'm working on for a particular project, but also it helps me keep them organized. 
+
+In this Weather Dashboard application you will be able to
+
+* Search by US city to find the current weather for that city
+* See a detailed five day forecast for that city
+* See in a list, the cities that you have previously searched in app
+
+Thank you again for reviewing my submission. 
+
+OG 👽☯️
--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,14 @@
-// var apiKey="8014a1ac0a4ac149beb5fec93b61ac5a"
-// var apiURL =
-//   "https://api.openweathermap.org/data/2.5/weather?q=Indianapolis&appid=" + apiKey;
-//   fetch(apiURL)
-//   .then(function(response){
-//     return response.json()
-//   })
-//   .then(function(data){
-
-//     console.log(data)
-//     var lat = data.coord.lat
-//     var lon = data.coord.lon
-
-//   var api5DayURL =
-//     "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=" + apiKey;
-//     fetch(api5DayURL)
-//     .then(function(response){
-//         return response.json()
-//     })
-//     .then(function(data){
-//         console.log(data)
-//     })
-// })
-
+// API KEY 
 var apiKey = "8014a1ac0a4ac149beb5fec93b61ac5a";
 
+
+// QUERY SELECTORS 
 var searchButton = document.querySelector("#search-button");
-
 var searchInput = document.querySelector("#search-input");
-
 var fiveDayForecastContainer = document.querySelector("#js-five-day-forecast")
+
+
+// FUNCTIONS TO GET API WEATHER DATA
 
 function geoCodingFetch(query) {
   var apiURL =
@@ -81,3 +61,4 @@ function renderForecastCard(title){
 
 fiveDayForecastContainer.appendChild(cardEl)
 }
+
